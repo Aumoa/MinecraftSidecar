adeb4ac8f9f3fa9ad9a2c4c7f3e8b3b5e04151a0
liberty <wkdghcjf1234@gmail.com>
2025-10-25 23:47:40 +0900

    Localization 지원

--- SidecarWeb/Startup.cs
+++ SidecarWeb/Startup.cs
@@ -15,6 +15,15 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddHttpClient();
 
+builder.Services.AddLocalization(o => o.ResourcesPath = "Localizational");
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    string[] supportedCultures = ["en", "ko"];
+    options.SetDefaultCulture("en")
+           .AddSupportedCultures(supportedCultures)
+           .AddSupportedUICultures(supportedCultures);
+});
+
 var dataProtection = builder.Configuration.GetSection("DataProtection");
 if (dataProtection.Exists())
 {
@@ -54,6 +63,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseRequestLocalization();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
